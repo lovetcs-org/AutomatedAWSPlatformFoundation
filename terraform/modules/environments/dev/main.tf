9dea6bd3acdb5b473d176e46913c762395c5e253
Lovet <102820462+lovetcs@users.noreply.github.com>
2026-01-14 17:36:37 +0000

    tf file set up

---
+++ terraform/modules/environments/dev/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+ }
+
+variable "aws_region" {
+    type = string
+    description = "Region currently deploying to."
+}
+
+provider "aws" {
+    region = var.aws_region
+}
+
+terraform {
+    backend "s3" {
+        bucket = "lovetcs-dev-lock-bucket"
+        key = "terraform.tfstate"
+        region = var.aws_region
+        encrypt = true
+        use_lockfile = true
+    }
+}
